8a0c4e056c48495e5abea9c9db4305f9ef351f23
William Zhang <W.YkZhang@hotmail.com>
2019-12-12 23:27:03 -0500

    added list of all learning rates

--- cifar.py
+++ cifar.py
@@ -280,78 +280,79 @@ if __name__ == '__main__':
                          if name.islower() and not name.startswith("__")
                          and callable(models.__dict__[name]))
 
-    learning_rate = 0.01
+    lr_simplified = [0.01, 0.02, 0.03, 0.05, 0.075, 0.10, 0.125, 0.15, 0.18, 0.20]
     batch_options = [32, 96, 128, 160, 192, 224, 256, 320]
     for batch in batch_options:
-        checkpoint = "checkpoints/cifar10/vgg19/LR_{}_BS_{}".format(learning_rate,batch)
-        parser = argparse.ArgumentParser(description='PyTorch CIFAR10/100 Training')
-        # Datasets
-        parser.add_argument('-d', '--dataset', default='cifar10', type=str)
-        parser.add_argument('-j', '--workers', default=4, type=int, metavar='N',
-                            help='number of data loading workers (default: 4)')
-        # Optimization options
-        parser.add_argument('--epochs', default=50, type=int, metavar='N',
-                            help='number of total epochs to run')
-        parser.add_argument('--start-epoch', default=0, type=int, metavar='N',
-                            help='manual epoch number (useful on restarts)')
-        parser.add_argument('--train-batch', default=batch, type=int, metavar='N',
-                            help='train batchsize')
-        parser.add_argument('--test-batch', default=batch, type=int, metavar='N',
-                            help='test batchsize')
-        parser.add_argument('--lr', '--learning-rate', default=learning_rate, type=float,
-                            metavar='LR', help='initial learning rate')
-        parser.add_argument('--drop', '--dropout', default=0, type=float,
-                            metavar='Dropout', help='Dropout ratio')
-        parser.add_argument('--schedule', type=int, nargs='+', default=[150, 225],
-                            help='Decrease learning rate at these epochs.')
-        parser.add_argument('--gamma', type=float, default=1, help='LR is multiplied by gamma on schedule.')
-        parser.add_argument('--momentum', default=0, type=float, metavar='M',
-                            help='momentum')
-        parser.add_argument('--weight-decay', '--wd', default=0, type=float,
-                            metavar='W', help='weight decay (default: 1e-4)')
-        # Checkpoints
-        parser.add_argument('-c', '--checkpoint', default='checkpoints/cifar10/vgg19', type=str, metavar='PATH',
-                            help='path to save checkpoint (default: checkpoint)')
-        parser.add_argument('--resume', default='', type=str, metavar='PATH',
-                            help='path to latest checkpoint (default: none)')
-        # Architecture
-        parser.add_argument('--arch', '-a', metavar='ARCH', default='vgg19',
-                            choices=model_names,
-                            help='model architecture: ' +
-                                 ' | '.join(model_names) +
-                                 ' (default: resnet18)')
-        parser.add_argument('--depth', type=int, default=29, help='Model depth.')
-        parser.add_argument('--block-name', type=str, default='BasicBlock',
-                            help='the building block for Resnet and Preresnet: BasicBlock, Bottleneck (default: Basicblock for cifar10/cifar100)')
-        parser.add_argument('--cardinality', type=int, default=8, help='Model cardinality (group).')
-        parser.add_argument('--widen-factor', type=int, default=4, help='Widen factor. 4 -> 64, 8 -> 128, ...')
-        parser.add_argument('--growthRate', type=int, default=12, help='Growth rate for DenseNet.')
-        parser.add_argument('--compressionRate', type=int, default=2, help='Compression Rate (theta) for DenseNet.')
-        # Miscs
-        parser.add_argument('--manualSeed', type=int, help='manual seed')
-        parser.add_argument('-e', '--evaluate', dest='evaluate', action='store_true',
-                            help='evaluate model on validation set')
-        # Device options
-        parser.add_argument('--gpu-id', default='0', type=str,
-                            help='id(s) for CUDA_VISIBLE_DEVICES')
+        for learning_rate in lr_simplified:
+            checkpoint = "checkpoints/cifar10/vgg19/LR_{}_BS_{}".format(learning_rate,batch)
+            parser = argparse.ArgumentParser(description='PyTorch CIFAR10/100 Training')
+            # Datasets
+            parser.add_argument('-d', '--dataset', default='cifar10', type=str)
+            parser.add_argument('-j', '--workers', default=4, type=int, metavar='N',
+                                help='number of data loading workers (default: 4)')
+            # Optimization options
+            parser.add_argument('--epochs', default=50, type=int, metavar='N',
+                                help='number of total epochs to run')
+            parser.add_argument('--start-epoch', default=0, type=int, metavar='N',
+                                help='manual epoch number (useful on restarts)')
+            parser.add_argument('--train-batch', default=batch, type=int, metavar='N',
+                                help='train batchsize')
+            parser.add_argument('--test-batch', default=batch, type=int, metavar='N',
+                                help='test batchsize')
+            parser.add_argument('--lr', '--learning-rate', default=learning_rate, type=float,
+                                metavar='LR', help='initial learning rate')
+            parser.add_argument('--drop', '--dropout', default=0, type=float,
+                                metavar='Dropout', help='Dropout ratio')
+            parser.add_argument('--schedule', type=int, nargs='+', default=[150, 225],
+                                help='Decrease learning rate at these epochs.')
+            parser.add_argument('--gamma', type=float, default=1, help='LR is multiplied by gamma on schedule.')
+            parser.add_argument('--momentum', default=0, type=float, metavar='M',
+                                help='momentum')
+            parser.add_argument('--weight-decay', '--wd', default=0, type=float,
+                                metavar='W', help='weight decay (default: 1e-4)')
+            # Checkpoints
+            parser.add_argument('-c', '--checkpoint', default='checkpoints/cifar10/vgg19', type=str, metavar='PATH',
+                                help='path to save checkpoint (default: checkpoint)')
+            parser.add_argument('--resume', default='', type=str, metavar='PATH',
+                                help='path to latest checkpoint (default: none)')
+            # Architecture
+            parser.add_argument('--arch', '-a', metavar='ARCH', default='vgg19',
+                                choices=model_names,
+                                help='model architecture: ' +
+                                     ' | '.join(model_names) +
+                                     ' (default: resnet18)')
+            parser.add_argument('--depth', type=int, default=29, help='Model depth.')
+            parser.add_argument('--block-name', type=str, default='BasicBlock',
+                                help='the building block for Resnet and Preresnet: BasicBlock, Bottleneck (default: Basicblock for cifar10/cifar100)')
+            parser.add_argument('--cardinality', type=int, default=8, help='Model cardinality (group).')
+            parser.add_argument('--widen-factor', type=int, default=4, help='Widen factor. 4 -> 64, 8 -> 128, ...')
+            parser.add_argument('--growthRate', type=int, default=12, help='Growth rate for DenseNet.')
+            parser.add_argument('--compressionRate', type=int, default=2, help='Compression Rate (theta) for DenseNet.')
+            # Miscs
+            parser.add_argument('--manualSeed', type=int, help='manual seed')
+            parser.add_argument('-e', '--evaluate', dest='evaluate', action='store_true',
+                                help='evaluate model on validation set')
+            # Device options
+            parser.add_argument('--gpu-id', default='0', type=str,
+                                help='id(s) for CUDA_VISIBLE_DEVICES')
 
-        args = parser.parse_args()
-        state = {k: v for k, v in args._get_kwargs()}
+            args = parser.parse_args()
+            state = {k: v for k, v in args._get_kwargs()}
 
-        # Validate dataset
-        assert args.dataset == 'cifar10' or args.dataset == 'cifar100', 'Dataset can only be cifar10 or cifar100.'
+            # Validate dataset
+            assert args.dataset == 'cifar10' or args.dataset == 'cifar100', 'Dataset can only be cifar10 or cifar100.'
 
-        # Use CUDA
-        os.environ['CUDA_VISIBLE_DEVICES'] = args.gpu_id
-        use_cuda = torch.cuda.is_available()
+            # Use CUDA
+            os.environ['CUDA_VISIBLE_DEVICES'] = args.gpu_id
+            use_cuda = torch.cuda.is_available()
 
-        # Random seed
-        if args.manualSeed is None:
-            args.manualSeed = random.randint(1, 10000)
-        random.seed(args.manualSeed)
-        torch.manual_seed(args.manualSeed)
-        if use_cuda:
-            torch.cuda.manual_seed_all(args.manualSeed)
-        best_acc = 0  # best test accuracy
+            # Random seed
+            if args.manualSeed is None:
+                args.manualSeed = random.randint(1, 10000)
+            random.seed(args.manualSeed)
+            torch.manual_seed(args.manualSeed)
+            if use_cuda:
+                torch.cuda.manual_seed_all(args.manualSeed)
+            best_acc = 0  # best test accuracy
 
-        main()
+            main()
